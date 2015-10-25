804266d6d729f835fee5955c3a44bc16189eb468
Nerijus <nyrkao@gmail.com>
2015-10-25 14:23:06 +0200

    Fix for "Char 0x0 out of allowed range simplexml_load_file" issue

--- src/parsers.php
+++ src/parsers.php
@@ -72,7 +72,8 @@ if (!class_exists('WXR_Parser')) {
             $authors = $posts = $categories = $tags = $terms = array();
 
             $internal_errors = libxml_use_internal_errors(true);
-            $xml = simplexml_load_file($file);
+            stream_filter_register('xmlutf8', 'ValidUTF8XMLFilter');
+            $xml = simplexml_load_file('php://filter/read=xmlutf8/resource=' . $file);
             // halt if loading produces an error
             if (!$xml) {
                 return new WP_Error('SimpleXML_parse_error', 'There was an error when reading this WXR file',
@@ -757,6 +758,26 @@ if (!class_exists('WXR_Parser')) {
                 return gzclose($fp);
             }
             return fclose($fp);
+        }
+    }
+
+    /**
+     * Class ValidUTF8XMLFilter
+     * @FIX: Char 0x0 out of allowed range simplexml_load_file issue
+     * @see: http://stackoverflow.com/questions/3466035/how-to-skip-invalid-characters-in-xml-file-using-php
+     */
+    class ValidUTF8XMLFilter extends php_user_filter
+    {
+        protected static $pattern = '/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]+/u';
+
+        public function filter($in, $out, &$consumed, $closing)
+        {
+            while ($bucket = stream_bucket_make_writeable($in)) {
+                $bucket->data = preg_replace(self::$pattern, '', $bucket->data);
+                $consumed += $bucket->datalen;
+                stream_bucket_append($out, $bucket);
+            }
+            return PSFS_PASS_ON;
         }
     }
 }
